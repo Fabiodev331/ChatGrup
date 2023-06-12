eebc30f9df73a099d6b3071b56f1ab525161a8fb
devprogramador <sjosefabio1996@gmail.com>
2023-06-12 13:29:24 -0300

    pagina de login e senha criadas

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,11 +1,59 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, {useState} from "react";
+import { View, Text,TouchableOpacity, Platform } from "react-native";
+import { 
+  Container,
+  Logo,
+  Title,
+  Input,
+  ButtonArea,
+  ButtonText
+} from "./styles";
 
 function SignIn(){
+  const [nome, setNome] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [type, setType] = useState(false);
+
   return(
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}} >
-      <Text>Chatgrupo Login</Text>
-    </View>
+    <Container>
+      
+      <Logo style={{ marginTop: Platform.OS === 'android' ? 60 : 80 }} >ChatGrup</Logo>
+      <Title>Ajude, colabore, faça networking!</Title>
+
+      { type && <Input
+        placeholder="Seu nome"
+        placeholderTextColor="#69696b"
+        value={nome}
+        onChangeText={(text) => setNome(text)}
+        />
+      }
+
+      <Input
+      placeholder="Seu email"
+      placeholderTextColor="#69696b"
+      value={email}
+      onChangeText={(text) => setEmail(text)}
+      />
+
+      <Input
+      placeholder="******"
+      placeholderTextColor="#69696b"
+      value={password}
+      secureTextEntry={true}
+      onChangeText={(text) => setPassword(text)}
+      />
+
+      <ButtonArea style={{ backgroundColor: type ? "#f53745" : "#57dd86" }} >
+        <ButtonText>{type ? "Cadastrar" : "Acessar"}</ButtonText>
+      </ButtonArea>
+
+      <TouchableOpacity onPress={() => setType(!type)} >
+        <Text>{type ? "Já possuo uma conta" : "Criar uma nova conta"}</Text>
+      </TouchableOpacity>
+
+
+    </Container>
   )
 }
 
