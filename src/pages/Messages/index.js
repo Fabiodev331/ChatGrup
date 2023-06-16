7c03e50510470679b9f273842e0efd97b6d4affa
devprogramador <sjosefabio1996@gmail.com>
2023-06-16 13:56:30 -0300

    buscando mensagens no banco

--- src/pages/Messages/index.js
+++ src/pages/Messages/index.js
@@ -1,8 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text } from "react-native";
+
+import auth from "@react-native-firebase/auth";
+import firestore from "@react-native-firebase/firestore";
 
 function Messages({ route }){
   const { thread } = route.params;
+  const [message, setMessage] = useState([]);
+
+  const user = auth().currentUser.toJSON();
+
+  useEffect( () => {
+    const unsubscribeListener = firestore().collection('MESSAGE_THREADS')
+    .doc(thread._id)
+    .collection('MESSAGE')
+    .orderBy('createdAt', 'desc')
+    .onSnapshot( querySnapshot => {
+      const messages = querySnapshot.docs.map( doc => {
+        const firebaseData = doc.data()
+
+        const data = {
+          _id: doc.id,
+          text: '',
+          createdAt: firestore.FieldValue.serverTimestamp(),
+          ...firebaseData
+        }
+
+        if(!firebaseData.system){
+          data.user = {
+            ...firebaseData.user,
+            name: firebaseData.user.displayName
+          }
+        }
+
+        return data;
+
+      })
+      setMessage(messages);
+    })
+
+    return () => unsubscribeListener();
+  }, [] );
+
   return(
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}} >
       <Text>{ thread.name }</Text>
